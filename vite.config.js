8cdd687a4f61b76665141d50259766a0a34eb437
mayankgarg00 <mayankgargipad@gmail.com>
2026-03-10 12:50:30 +0530

    edits

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,23 @@ import { defineConfig } from 'vite'
 import glsl from 'vite-plugin-glsl'
 
 export default defineConfig({
-    plugins: [glsl()]
+    plugins: [glsl()],
+    build: {
+        outDir: 'dist',
+        sourcemap: false,
+        minify: 'terser',
+        terserOptions: {
+            compress: true,
+            mangle: true
+        },
+        rollupOptions: {
+            output: {
+                manualChunks: undefined
+            }
+        }
+    },
+    server: {
+        port: 5173,
+        strictPort: false
+    }
 })
